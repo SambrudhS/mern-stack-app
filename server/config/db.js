61fabe2036743f07c2a6375f8d5138ee96a770b8
pragathi <pragathisharathkumar1909@gmail.com>
2024-09-19 12:41:59 +0530

    Initial commit for MERN stack project

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const MONGO_URI="mongodb+srv://admin:admin1@test1.9ifty.mongodb.net/?retryWrites=true&w=majority&appName=Test1"
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
